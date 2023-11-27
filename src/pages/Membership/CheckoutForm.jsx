5a87bdf8c321f3ca7843966491f884afa4955b86
shihab01118 <shahed.shihab.0@gmail.com>
2023-11-28 02:02:38 +0600

    membership page added with required functionalities

---
+++ src/pages/Membership/CheckoutForm.jsx
@@ -0,0 +1,126 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axiosSecure from "../../api";
+import useAuth from "../../hooks/useAuth";
+import useUsers from "../../hooks/useUsers";
+import { Button } from "@mui/material";
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [errorMessage, setErrorMessage] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const [transactionId, setTransactionId] = useState("");
+  const { user } = useAuth();
+  const { refetch } = useUsers();
+
+  useEffect(() => {
+    axiosSecure.post("/create-payment-intent", { money: 20 }).then((res) => {
+      const data = res.data;
+      console.log(res.data);
+      setClientSecret(data.clientSecret);
+    });
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (card === null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("payment error", error);
+      setErrorMessage(error.message);
+    } else {
+      console.log("payment method", paymentMethod);
+      setErrorMessage("");
+    }
+
+    const { paymentIntent, error: paymentError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || "anonymous",
+            name: user?.name || "anonymous",
+          },
+        },
+      });
+
+    if (paymentError) {
+      console.log("payment error", paymentError);
+    } else {
+      console.log("payment intent", paymentIntent);
+      if (paymentIntent.status === "succeeded") {
+        console.log("transaction Id : ", paymentIntent.id);
+        setTransactionId(paymentIntent.id);
+
+        const goldUser = {
+          paymentSuccess: true,
+          transactionId: paymentIntent.id,
+        };
+
+        const { data } = await axiosSecure.patch(
+          `/users/update_badge/${user?.email}`,
+          goldUser
+        );
+        console.log(data);
+        if (data.transactionId) {
+          refetch();
+          toast.success("Congratulations! Achived Gold Badge.");
+        }
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: "16px",
+              color: "#424770",
+              "::placeholder": {
+                color: "#aab7c4",
+              },
+            },
+            invalid: {
+              color: "#9e2146",
+            },
+          },
+        }}
+      />
+      <p className="mt-6 text-red-600">{errorMessage}</p>
+      <Button
+        variant="contained"
+        style={{display: "block", margin: "6px auto"}}
+        // className="block mx-auto mt-6 btn btn-sm btn-primary w-24 capitalize"
+        type="submit"
+        disabled={!stripe || !clientSecret}
+      >
+        Pay
+      </Button>
+      {transactionId && (
+        <p className="text-green-600">
+          Your transaction id is: {transactionId}
+        </p>
+      )}
+    </form>
+  );
+};
+export default CheckoutForm;
--- src/pages/Membership/Membership.jsx
+++ src/pages/Membership/Membership.jsx
@@ -1,9 +1,30 @@
-const Membership = () => {
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { Helmet } from "react-helmet-async";
+import CheckoutForm from "./CheckoutForm";
+import Title from "../../components/Dashboard/Title";
+import Container from "../../components/shared/Container";
+import { Card } from "@mui/material";
 
-    return (
-        <div className="pt-[78px]">
-            <p> Hello, I am Membership </p>
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
+
+const Membership = () => {
+  return (
+    <div className="pt-[78px] h-[calc(100vh-78px)] flex items-center justify-center">
+      <Helmet>
+        <title>DiscussHub | Membership</title>
+      </Helmet>
+      <Container>
+        <div className="m-10">
+        <Title heading="Unlock Limitless Posting" subheading="Post Freely and Amplify Your Voice with Gold Access" />
+          <Card style={{width: "550px", margin: "40px auto", padding: "32px"}} elevation={4}>
+            <Elements stripe={stripePromise}>
+              <CheckoutForm />
+            </Elements>
+          </Card>
         </div>
-    )
+      </Container>
+    </div>
+  );
 };
 export default Membership;
