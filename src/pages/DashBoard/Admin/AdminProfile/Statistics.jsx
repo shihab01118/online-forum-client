423581fcea6588abd3f5982d77f864cb74d66f74
shihab01118 <shahed.shihab.0@gmail.com>
2023-11-29 11:53:23 +0600

    statistics with pie chart added

--- src/pages/DashBoard/Admin/AdminProfile/Statistics.jsx
+++ src/pages/DashBoard/Admin/AdminProfile/Statistics.jsx
@@ -1,12 +1,22 @@
 import PropTypes from "prop-types";
-// import Chart from "react-apexcharts";
+import Chart from "react-apexcharts";
 
 const Statistics = ({ userCount, postCount, commentCount }) => {
-  console.log(userCount, postCount, commentCount);
 
   return (
-    <div>
-      {/* <Chart></Chart> */}
+    <div className="mt-[55px]">
+      <h2 className="text-xl font-semibold">Statistics</h2>
+      <div className="w-fit mx-auto">
+      <Chart
+        options={{
+          labels: ["Total Users", "Total Posts", "Total Comments"]
+        }}
+        series={[userCount, postCount, commentCount]}
+        type="pie"
+        width="500"
+        height={500}
+      />
+      </div>
     </div>
   );
 };
--- src/pages/DashBoard/Admin/AdminProfile/Stats.jsx
+++ src/pages/DashBoard/Admin/AdminProfile/Stats.jsx
@@ -4,7 +4,7 @@ import { FaComment, FaList, FaUsers } from "react-icons/fa";
 const Stats = ({ userCount, postCount, commentCount }) => {
   return (
     <div className="grid md:grid-cols-3 gap-4">
-      <div className="bg-gradient-to-r from-[#0096c7] to-[#caf0f8] h-28 rounded shadow-md text-white text-center flex justify-center items-center">
+      <div className="bg-gradient-to-r from-[#0096c7] to-[#90D6EA] h-28 rounded shadow-md text-white text-center flex justify-center items-center">
         <div>
           <h3 className="text-xl font-semibold mb-3">Total Users</h3>
           <div className="w-fit mx-auto">
@@ -15,7 +15,7 @@ const Stats = ({ userCount, postCount, commentCount }) => {
           </div>
         </div>
       </div>
-      <div className="bg-gradient-to-r from-[#67729D] to-[#FED9ED] h-28 rounded shadow-md text-white flex justify-center items-center">
+      <div className="bg-gradient-to-r from-[#06BC6B] to-[#49F3B5] h-28 rounded shadow-md text-white flex justify-center items-center">
         <div>
           <h3 className="text-xl font-semibold mb-3">Total Posts</h3>
           <div className="w-fit mx-auto">
@@ -26,7 +26,7 @@ const Stats = ({ userCount, postCount, commentCount }) => {
           </div>
         </div>
       </div>
-      <div className="bg-gradient-to-r from-[#FF9D08] to-[#FFF49E] h-28 rounded shadow-md text-white flex justify-center items-center">
+      <div className="bg-gradient-to-r from-[#FF9D08] to-[#FFD008] h-28 rounded shadow-md text-white flex justify-center items-center">
         <div>
           <h3 className="text-xl font-semibold mb-3">Total Comments</h3>
           <div className="w-fit mx-auto">
